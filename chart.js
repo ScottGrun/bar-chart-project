40262270576a0737defec85037480924a47679d5
ScottGrun <45321659+ScottGrun@users.noreply.github.com>
2020-07-11 15:42:38 -0400

    Height now determined by data given / values added a <p> to bars

--- chart.js
+++ chart.js
@@ -16,19 +16,40 @@ const setSize = function (element, width, height) {
   document.querySelector(element).style.height = height.toString() + "px";
 };
 
-const renderBars = function (data, element) {
+const renderBars = function (data) {
   for (let i = 0; i < data.length; i++) {
     let bar = document.createElement("div");
     let barWidth = options.width / data.length;
     let gapBetweenBars = barWidth * 0.05;
-
     barWidth = barWidth - gapBetweenBars;
     bar.style.width = barWidth.toString() + "px";
-    //this is temporary make this dynamic
-    bar.style.height = "100px";
-    bar.style.margin = gapBetweenBars.toString() + "px";
+    let margin = `0px ${gapBetweenBars.toString()}px`;
+    bar.style.margin = margin;
     bar.style.backgroundColor = "red";
+    bar.className = "barElement";
+
+    let barValue = document.createElement("p");
+    barValue.append(data[i]);
+    bar.append(barValue);
+    barValue.style.textAlign = "center";
     document.getElementById("chart").appendChild(bar);
+  }
+  setBarHeight(data);
+};
+
+const setBarHeight = function (data) {
+  let bars = document.getElementsByClassName("barElement");
+  let largestElemet = data[0];
+  for (let i = 0; i < data.length; i++) {
+    if (largestElemet < data[i]) {
+      largestElemet = data[i];
+    }
+  }
+
+  console.log(`Largest ${largestElemet}`);
+  for (let i = 0; i < bars.length; i++) {
+    let barHeight = (data[i] / largestElemet) * 100;
+    bars[i].style.height = barHeight.toString() + "%";
   }
 };
 
@@ -36,6 +57,7 @@ const setBackgroundColor = function (element, color) {
   document.querySelector(element).style.backgroundColor = color;
 };
 
-drawBarChart([1, 2, 3, 4, 5, 5, 1, 2], options, "#chart");
+drawBarChart([2, 12, 41, 24, 21, 3, 9, 5, 5, 15, 2], options, "#chart");
 
 //test zone
+//handle negative numbers
