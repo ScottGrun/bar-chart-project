108e31e29caf3ea1856e49c96877808fdf0e720a
ScottGrun <45321659+ScottGrun@users.noreply.github.com>
2020-07-12 13:45:17 -0400

    Added function to create y-axis labels as multiples of 5

--- chart.js
+++ chart.js
@@ -2,13 +2,19 @@ let options = {
   width: 800,
   height: 500,
   backgroundColor: "white",
+  bars: {
+    color: "blue",
+    labelColor: "white",
+    gap: "8",
+  },
 };
 
 const drawBarChart = function (data, options, element) {
   let chartBody = document.getElementById(element);
   setSize("#chart", options.width, options.height);
   setBackgroundColor("#chart", options.backgroundColor);
-  renderBars(data, chartBody);
+  renderBars(data, options.bars);
+  generateYAxisLabels(data);
 };
 
 const setSize = function (element, width, height) {
@@ -16,24 +22,33 @@ const setSize = function (element, width, height) {
   document.querySelector(element).style.height = height.toString() + "px";
 };
 
-const renderBars = function (data) {
+const renderBars = function (data, barOptions) {
   for (let i = 0; i < data.length; i++) {
+    //Create and Size Bar Element
     let bar = document.createElement("div");
     let barWidth = options.width / data.length;
-    let gapBetweenBars = barWidth * 0.05;
+    let gapBetweenBars = barWidth * (barOptions.gap * 0.01);
     barWidth = barWidth - gapBetweenBars;
     bar.style.width = barWidth.toString() + "px";
     let margin = `0px ${gapBetweenBars.toString()}px`;
     bar.style.margin = margin;
-    bar.style.backgroundColor = "red";
     bar.className = "barElement";
 
+    //Style Bar with User Options
+    bar.style.backgroundColor = barOptions.color;
+    bar.style.color = barOptions.labelColor;
+
+    //Create  & add data label to bar
     let barValue = document.createElement("p");
     barValue.append(data[i]);
     bar.append(barValue);
     barValue.style.textAlign = "center";
+
+    //Add bar to graph
     document.getElementById("chart").appendChild(bar);
   }
+
+  //Determine bar height based on data value
   setBarHeight(data);
 };
 
@@ -48,8 +63,31 @@ const setBarHeight = function (data) {
 
   console.log(`Largest ${largestElemet}`);
   for (let i = 0; i < bars.length; i++) {
-    let barHeight = (data[i] / largestElemet) * 100;
+    let barHeight = (data[i] / (Math.ceil(largestElemet / 5) * 5)) * 100;
     bars[i].style.height = barHeight.toString() + "%";
+  }
+};
+
+const generateYAxisLabels = function (data) {
+  let yAxisLabels = document.createElement("div");
+  yAxisLabels.id = "yAxisLabels";
+  document.getElementById("chart").appendChild(yAxisLabels);
+  let largestElemet = data[0];
+  for (let i = 0; i < data.length; i++) {
+    if (largestElemet < data[i]) {
+      largestElemet = data[i];
+    }
+  }
+  //Round to nearest multiple of 5 -- https://stackoverflow.com/questions/18953384/javascript-round-up-to-the-next-multiple-of-5
+  largestElemet = Math.ceil(largestElemet / 5) * 5;
+  for (let i = largestElemet; i >= 0; i -= 5) {
+    let yLabel = document.createElement("p");
+    // let yLabelData = Math.trunc(
+    //   (largestElemet / (data.length / largestElemet)) * i
+    // );
+
+    yLabel.append(i);
+    document.getElementById("yAxisLabels").appendChild(yLabel);
   }
 };
 
@@ -57,58 +95,7 @@ const setBackgroundColor = function (element, color) {
   document.querySelector(element).style.backgroundColor = color;
 };
 
-drawBarChart(
-  [
-    2,
-    24,
-    21,
-    3,
-    9,
-    5,
-    5,
-    12,
-    41,
-    24,
-    21,
-    3,
-    9,
-    5,
-    5,
-    24,
-    21,
-    24,
-    21,
-    3,
-    9,
-    5,
-    5,
-    3,
-    9,
-    5,
-    5,
-    15,
-    2,
-    12,
-    41,
-    24,
-    21,
-    3,
-    9,
-    5,
-    5,
-    21,
-    12,
-    41,
-    24,
-    21,
-    3,
-    9,
-    5,
-    5,
-  ],
-  options,
-  "#chart"
-);
+drawBarChart([1, 2, 4, 12, 30, 42, 21, 5, 6, 7, 8, 10], options, "#chart");
 
 //test zone
 //handle negative numbers
